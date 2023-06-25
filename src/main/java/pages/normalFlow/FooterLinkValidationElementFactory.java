01907069fffe5d17b09e869f9a23cefa6d249c30
Santanil <santanilbasak@gmail.com>
2023-06-25 23:23:44 +0530

    Initial commit
    Signed-off-by: Santanil <santanilbasak@gmail.com>

---
+++ src/main/java/pages/normalFlow/FooterLinkValidationElementFactory.java
@@ -0,0 +1,18 @@
+package pages.normalFlow;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import utils.TestNGCreation;
+
+public class FooterLinkValidationElementFactory extends TestNGCreation{
+	
+	
+//	@FindBy(xpath = "(//table[@class='gf-t']//ul)[1]/li")
+//	public static List<WebElement> footerLinks;
+//	
+	@FindBy(xpath = "//h1[text()='Practice Page']")
+	public static WebElement practicePageLabel;
+}
